00e833ab0108bf53143589c8ec83ef7e6f9b2b14
takacsberni <takacsberni@gmail.com>
2022-09-15 17:54:48 +0200

    feat: we get all the necessarily data based on our latitude and longitude

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+
 export default function App() {
 
   const [lat, setLat] = useState({});
   const [long, setLong] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
+      const fetchData = async () => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        });
+        await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+            setData(result)
+            console.log(result);
+          });
+      }
+      fetchData();
   }, [lat, long]);
+
 
   return (
       <div className="App">
