dfc9077fac251911bb6434c39bfb5d0828372b09
takacsberni <takacsberni@gmail.com>
2022-09-15 18:14:38 +0200

    feat: App further elements to show within the Weather component

--- weather/src/components/weather.jsx
+++ weather/src/components/weather.jsx
@@ -5,8 +5,12 @@ const WeatherCard = ( {weatherData}) => (
     <Card>
         <Card.Content>
             <Card.Header className="header">
-                {weatherData.name}
+                City name: {weatherData.name}
             </Card.Header>
+            <p> Temperature: {weatherData.main.temp} </p>
+            <p> Sunrise: {weatherData.sys.sunrise} </p>
+            <p> Sunset: {weatherData.sys.sunset} </p>
+            <p> Description: {weatherData.weather[0].description} </p>
         </Card.Content>
     </Card>
 )
