5190b6f2923a60e9308fc28eec08e914a72e418f
takacsberni <takacsberni@gmail.com>
2022-09-15 18:23:49 +0200

    feat: add a button to refresh the page

--- weather/src/components/weather.jsx
+++ weather/src/components/weather.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import {Card} from 'semantic-ui-react';
+import {Button, Card} from 'semantic-ui-react';
 import moment from 'moment';
+
+const refresh = () => {
+    window.location.reload();
+}
 
 const WeatherCard = ( {weatherData}) => (
     <Card>
         <Card.Content>
+            <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
             <Card.Header className="header">
                 City name: {weatherData.name}
             </Card.Header>
